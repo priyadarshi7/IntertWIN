215cdbc0d081576194b6e22a12c933e9dd49bc4e
priyadarshi7 <priyadarshisatyakam3@gmail.com>
2024-10-19 15:29:39 +0530

    changes

--- project/blockchain/migrations/2_deploy_marketplace.js
+++
@@ -1,7 +0,0 @@
-// blockchain/migrations/2_deploy_marketplace.js
-
-const Marketplace = artifacts.require('Marketplace');
-
-module.exports = function (deployer) {
-    deployer.deploy(Marketplace);
-};
